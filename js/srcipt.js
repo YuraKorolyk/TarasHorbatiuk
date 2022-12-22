84577da3f96bbe85eaa00350ba45eb8931ae11e5
Yurii <yura.korolyk@gmail.com>
2022-12-22 02:48:51 +0200

    articles

--- js/srcipt.js
+++ js/srcipt.js
@@ -67,40 +67,27 @@ window.onscroll = () => {
 
 
 
+// articles
+const articlesName = document.querySelectorAll(".articles__name");
+const articlesText = document.querySelectorAll(".articles__text");
 
-(() => {
-   const mobileMenu = document.querySelector(".js-menu-container");
-   const openMenuBtn = document.querySelector(".js-open-menu");
-   const closeMenuBtn = document.querySelector(".js-close-menu");
-   const body = document.querySelector('body');
- 
-   const toggleMenu = () => {
-     const isMenuOpen =
-       openMenuBtn.getAttribute("aria-expanded") === "true" || false;
-     openMenuBtn.setAttribute("aria-expanded", !isMenuOpen);
-     mobileMenu.classList.toggle("is-open");
-     body.classList.toggle("lock");
- 
-     const scrollLockMethod = !isMenuOpen
-       ? "disableBodyScroll"
-       : "enableBodyScroll";
-     bodyScrollLock[scrollLockMethod](document.body);
-   };
- 
-   openMenuBtn.addEventListener("click", toggleMenu);
-   closeMenuBtn.addEventListener("click", toggleMenu);
- 
-   // Close the mobile menu on wider screens if the device orientation changes
-   window.matchMedia("(min-width: 768px)").addEventListener("change", (e) => {
-     if (!e.matches) return;
-     mobileMenu.classList.remove("is-open");
-     openMenuBtn.setAttribute("aria-expanded", false);
-     bodyScrollLock.enableBodyScroll(document.body);
-   });
- })();
-
-
-
-
-
-
+articlesName.forEach((item) => {
+  item.addEventListener("click", (e) => {
+    const content = e.target.nextElementSibling;
+    if (content.style.maxHeight) {
+      closeAcord();
+    } else {
+      closeAcord();
+      item.classList.add("active");
+      content.style.maxHeight = content.scrollHeight + "px";
+    }
+  });
+});
+function closeAcord() {
+  articlesText.forEach((item) => {
+    item.style.maxHeight = null;
+  });
+  articlesName.forEach(item => {
+   item.classList.remove("active");
+});
+}
